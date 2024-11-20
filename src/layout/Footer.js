5d7be7f389aff094e07e42743dfbe021ccaf8589
inseo99 <jjj990626@naver.com>
2024-11-20 12:06:05 +0900

    간단한 리액트 예제로 화면 보기

---
+++ src/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    
+    return (
+        <footer>
+            <hr/>
+            하단 영역입니다.
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/layout/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    
+    return (
+        <header>
+            <a href="/">홈</a> &nbsp;&nbsp;
+            <a href="/boardWriteReact">게시판</a> &nbsp;&nbsp;
+            <hr/>
+        </header>
+    );
+};
+
+export default Header;
